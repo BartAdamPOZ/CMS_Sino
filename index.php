b466bfdc86b40b11d4d6c8f38c7c4a052c23439a
Bartlomiej Adamczewski <bartlomiej.adamczewski@gmail.com>
2023-09-29 20:46:32 +0200

    Added file index.php with basic structure and included required files like init/php , db.php, header.php and footer.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+require 'includes/init.php';
+
+$conn = require 'includes/db.php';
+
+require 'includes/header.php';
+
+?>
+
+<main>
+
+
+
+
+</main>
+
+<?php require 'includes/footer.php' ?>
