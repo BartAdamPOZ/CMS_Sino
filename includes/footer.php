68b89d05c743a37a31d105ee65004e8bb8e67bb2
Bartlomiej Adamczewski <bartlomiej.adamczewski@gmail.com>
2023-09-29 20:33:57 +0200

    Added basic files like styles.css , header.php , header.php , script.js and also file db.php which is responisble for connection with the database.

---
+++ includes/footer.php
@@ -0,0 +1,16 @@
+
+
+
+<script
+  src="https://code.jquery.com/jquery-3.7.1.min.js"
+  integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
+  crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/jquery.validate.min.js"></script>
+  <script src="/js/script.js"></script>
+</body>
+
+<footer>
+
+</footer>
+
+</html>
