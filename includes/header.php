0e3a2d09201a963feacdfd88ca1168f5fe361ebd
Bartlomiej Adamczewski <bartlomiej.adamczewski@gmail.com>
2023-09-30 00:25:36 +0200

    Added site with comparison of plans

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
   <link rel="stylesheet" href="/css/styles.css">
 
   <title>Sino</title>
@@ -43,7 +44,7 @@
               <li><a class="dropdown-item" href="#">Osoby Kontaktowe</a></li>
             </ul>
           </li>
-        <li class="nav-item"><a href="#" class="nav-link">Subskrypcje</a></li>
+        <li class="nav-item"><a href="/includes/plans.php" class="nav-link">Subskrypcje</a></li>
       </ul>
     </header>
   </div>
---
+++ includes/plans.php
@@ -0,0 +1,72 @@
+<?php 
+
+require 'init.php';
+
+require 'header.php';
+
+
+?>
+
+  <h2 class="display-6 text-center mb-4">Co oferujemy?</h2>
+    <div class="table-responsive">
+      <table class="table text-center">
+        <thead>
+          <tr>
+            <th style="width: 34%;"></th>
+            <th style="width: 22%;">Podstawowy</th>
+            <th style="width: 22%;">Standard</th>
+            <th style="width: 22%;">Premium</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row" class="text-start">Wsparcie mentalne</th>
+            <td><i class="bi bi-check"></i></td>
+            <td><i class="bi bi-check"></i></td>
+            <td><i class="bi bi-check"></i></td>
+          </tr>
+          <tr>
+            <th scope="row" class="text-start">Dobrą atmosferę</th>
+            <td><i class="bi bi-check"></i></td>
+            <td><i class="bi bi-check"></i></td>
+            <td><i class="bi bi-check"></i></td>
+          </tr>
+        </tbody>
+
+        <tbody>
+          <tr>
+            <th scope="row" class="text-start">Rzetelność i uczciwość</th>
+            <td><i class="bi bi-check"></i></td>
+            <td><i class="bi bi-check"></i></td>
+            <td><i class="bi bi-check"></i></td>
+          </tr>
+          <tr>
+            <th scope="row" class="text-start">Pomoc mentora</th>
+            <td></td>
+            <td><i class="bi bi-check"></i></td>
+            <td><i class="bi bi-check"></i></td>
+          </tr>
+          <tr>
+            <th scope="row" class="text-start">Numer prywatny Prezesa</th>
+            <td></td>
+            <td></td>
+            <td><i class="bi bi-check"></i></td>
+          </tr>
+          <tr>
+            <th scope="row" class="text-start">Odpisywanie na maile w weekendy</th>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+
+
+
+
+
+
+
+<?php require 'footer.php'; ?>
