6ad127748f13f85e87b40cce58babf2e6a55ba7c
Bartlomiej Adamczewski <bartlomiej.adamczewski@gmail.com>
2023-09-30 17:21:36 +0200

    Changed placement of elements in company-site.php

--- company-site.php
+++ company-site.php
@@ -23,32 +23,32 @@ if (isset($_GET['id'])){
 
   <div class="container company-site">
     <div class="item1">
-    <?php if ($company): ?>
-      <h4>O firmie:</h4>
-      <h5>Nazwa: <?= htmlspecialchars($company -> name); ?></h5>
-      <p>Adres: <?= htmlspecialchars($company -> address); ?></p>
-      <p>Sektor: <?= htmlspecialchars($company -> sector); ?></p>
-      <p>Pakiet: <?= htmlspecialchars($company -> subscription); ?></p>
-    <?php else: ?>
+      <?php if ($company): ?>
+        <h4>O firmie:</h4>
+        <h5>Nazwa: <?= htmlspecialchars($company -> name); ?></h5>
+        <p>Adres: <?= htmlspecialchars($company -> address); ?></p>
+        <p>Sektor: <?= htmlspecialchars($company -> sector); ?></p>
+        <p>Pakiet: <?= htmlspecialchars($company -> subscription); ?></p>
+      <?php else: ?>
+        <p>Brak firmy w bazie danych.</p>
+      <?php endif; ?>
+    </div>
 
-    <?php endif; ?>
-  </div>
+    <div class="item2">
+      <h5>Dodaj opiekuna</h5>
+    </div>
 
-  <div class="item2">
-    <h5>Dodaj opiekuna</h5>
-  </div>
+    <div class="item3">
+      <h5>Dodaj nową osobę do kontaktu</h5>
+    </div>
 
-  <div class="item3">
-    <h5>Dodaj nową osobę do kontaktu</h5>
-  </div>
+    <div class="contact-table">
+      <h5>Tabela z osobami do kontaktu</h5>
+    </div>
 
-  <div class="contact-table">
-    <h5>Tabela z osobami do kontaktu</h5>
-  </div>
-
-  <div class="supervisor-table">
-    <h5>Tabela z opiekunami</h5>
-  </div>
+    <div class="supervisor-table">
+      <h5>Tabela z opiekunami</h5>
+    </div>
 
 
 
