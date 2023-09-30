860763fb5f5615b43e25dac6b22d0f573b60e943
Bartlomiej Adamczewski <bartlomiej.adamczewski@gmail.com>
2023-09-30 14:29:13 +0200

    Added new empty file company-site.php and also changed whole buttons on main site into links.

--- index.php
+++ index.php
@@ -11,20 +11,25 @@ require 'includes/header.php';
 
 <div class="container main-area">
   
+  <a href="new-employee-form.php" class="link-body-emphasis text-decoration-none">
     <div class="col-lg-4 col-sm-12 btn btn-main">
-      <h3><a href="new-employee-form.php" class="link-body-emphasis text-decoration-none">Dodaj Pracownika</a></h3>
+      <h3>Dodaj Pracownika</h3>
     </div>
+  </a>
    
   
+    <a href="new-client-form.php" class="link-body-emphasis text-decoration-none">
     <div class="col-lg-4 col-sm-12 btn btn-main">
-      <h3><a href="new-client-form.php" class="link-body-emphasis text-decoration-none">Dodaj Klienta</a></h3>
+      <h3>Dodaj Klienta</h3>
     </div>
+  </a>
    
-  
+  <a href="client-view.php" class="link-body-emphasis text-decoration-none">
     <div class="col-lg-4 col-sm-12 btn btn-main">
-      <h3><a href="client-view.php" class="link-body-emphasis text-decoration-none">Przeglądaj</a></h3>
+      <h3>Przeglądaj</h3>
     </div>
-  
+  </a>
+
 </div>
 
 
